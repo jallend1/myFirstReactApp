8ac50427a7e59c3548d8afa59b0cdc5b4fd7e81b
jallend1 <jallend1@gmail.com>
2020-03-04 18:29:20 -0800

    Removes unneeded files and adds Turtle component

---
+++ src/turtles.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Turtles extends Component{
+    render(){
+        return(
+            <div className="turtle">
+                <div>name: Leonardo</div>
+                <div>age: 1 </div>
+                <div>mask: blue</div>
+            </div>
+        )
+    }
+}
+
+export default Turtles
