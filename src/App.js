37edddb6ce6303369cd891b832b34de165965712
jallend1 <jallend1@gmail.com>
2020-03-05 17:49:36 -0800

    Adds list of turtles to pass through instead of displaying a static turtle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { Component} from 'react';
 import Turtles from './turtles';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My First React App!</h1>
-      <p>Welcome :D</p>
-      <Turtles />
-    </div>
-  );
+class App extends Component {
+  state = {
+    turtles: [
+      {name: 'Leonardo', mask: 'blue', id: 1},
+      {name: 'Donatello', mask: 'purple', id: 2},
+      {name: 'Raphael', mask: 'red', id: 3},
+      {name: 'Michelangelo', mask: 'orange', id: 4}
+    ]
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>My First React App!</h1>
+        <p>Welcome :D</p>
+        <Turtles turtles={this.state.turtles} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/turtles.js
+++ src/turtles.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 
 class Turtles extends Component{
     render(){
+        const { turtles } = this.props;
+        const turtleList = turtles.map(turtle => {
+            return(
+                <div className="turtle" key={ turtle.id }>
+                    <div>Name: { turtle.name } </div>
+                    <div>Mask: { turtle.mask } </div>
+                </div>
+            )
+        })
         return(
-            <div className="turtle">
-                <div>name: Leonardo</div>
-                <div>age: 1 </div>
-                <div>mask: blue</div>
+            <div className="turtlelist">
+                { turtleList }
             </div>
         )
     }
